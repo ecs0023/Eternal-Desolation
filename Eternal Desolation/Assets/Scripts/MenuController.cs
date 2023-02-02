04a00601177da0019c76a73ce62c00b37b82ff3a
ecs0023 <111142078+ecs0023@users.noreply.github.com>
2023-02-02 15:48:16 -0500

    Added Script to Both buttons to display StartGame and QuitGame
    Added Script to Both buttons to display StartGame and QuitGame

---
+++ Eternal Desolation/Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("GameStart");
+    }
+    public void EndGame()
+    {
+        Debug.Log("EndGame");
+    }
+}
